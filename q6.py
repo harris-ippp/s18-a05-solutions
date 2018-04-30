d417bf34d336d2db4b188c91b6b70b0ced4e3a5a
Eric Potash <eric@k2co3.net>
2018-04-29 21:25:36 -0500

    more weapon types

--- q6.py
+++ q6.py
@@ -2,5 +2,8 @@ import pandas as pd
 df = pd.read_csv('Crimes_-_2001_to_present.csv')
 
 weapon = (df.Description.str.find('WEAPON') > 0)
-no_weapon = (df.Description.str.find('NO WEAPON') > 0)
-print((weapon & no_weapon).mean())
+weapon = (df.Description.str.find('WEAPON') > 0)
+arm = (df.Description.str.find('ARM') > 0)
+gun = (df.Description.str.find('GUN') > 0)
+not_no_weapon = (df.Description.str.find('NO WEAPON') == -1)
+print(((weapon | arm | gun) & not_no_weapon).mean())
