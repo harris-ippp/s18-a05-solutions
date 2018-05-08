82731929bdfde5b6be5d14243ee19746cac7c020
Eric Potash <eric@k2co3.net>
2018-05-08 12:11:44 -0500

    duplicate

--- q6.py
+++ q6.py
@@ -2,7 +2,6 @@ import pandas as pd
 df = pd.read_csv('Crimes_-_2001_to_present.csv')
 
 weapon = (df.Description.str.find('WEAPON') > 0)
-weapon = (df.Description.str.find('WEAPON') > 0)
 arm = (df.Description.str.find('ARM') > 0)
 gun = (df.Description.str.find('GUN') > 0)
 not_no_weapon = (df.Description.str.find('NO WEAPON') == -1)
