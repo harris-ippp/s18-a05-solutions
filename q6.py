ea3099c5cdc9fbf83017d96fd8878982c9819a01
Eric Potash <eric@k2co3.net>
2018-05-08 12:12:59 -0500

    >=

--- q6.py
+++ q6.py
@@ -1,8 +1,8 @@
 import pandas as pd
 df = pd.read_csv('Crimes_-_2001_to_present.csv')
 
-weapon = (df.Description.str.find('WEAPON') > 0)
-arm = (df.Description.str.find('ARM') > 0)
-gun = (df.Description.str.find('GUN') > 0)
+weapon = (df.Description.str.find('WEAPON') >= 0)
+arm = (df.Description.str.find('ARM') >= 0)
+gun = (df.Description.str.find('GUN') >= 0)
 not_no_weapon = (df.Description.str.find('NO WEAPON') == -1)
 print(((weapon | arm | gun) & not_no_weapon).mean())
